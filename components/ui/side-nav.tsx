d48dcd7d6e3dad9a9e694021cf0d42421907d5a2
Taylor Lo <taylorloph@gmail.com>
2024-08-04 12:26:59 -0400

    Side-bar component functionality done

--- components/ui/side-nav.tsx
+++ components/ui/side-nav.tsx
@@ -2,26 +2,27 @@ import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineStars } from "react-icons/md";
 
 import React from "react";
+import Link from "next/link";
 
 export default function SideNav() {
   const Menus = [
-    { title: "Label", icon: <MdOutlineStars />, path: "" },
-    { title: "Label", icon: <MdOutlineStars />, path: "" },
-    { title: "Label", icon: <MdOutlineStars />, path: "" },
-    { title: "Label", icon: <MdOutlineStars />, path: "" },
+    { title: "Label", icon: <MdOutlineStars />, path: "/label1" },
+    { title: "Label", icon: <MdOutlineStars />, path: "/label2" },
+    { title: "Label", icon: <MdOutlineStars />, path: "/label3" },
+    { title: "Label", icon: <MdOutlineStars />, path: "/label4" },
   ];
 
   return (
     <div className="bg-light-violet h-screen p-3 pt-12 relative">
       <div className="rounded-2xl hover:bg-light-pink cursor-pointer p-4">
-        <AiOutlineHome className="text-3xl" />
+        <Link href="/"><AiOutlineHome className="text-3xl" /></Link>
       </div>
       <ul className="pt-2">
         {Menus.map((menu, index) => (
           <li className="text-center" key={index}>
-            <div className="rounded-2xl hover:bg-light-pink cursor-pointer p-4 flex flex-col items-center gap-2">
+            <Link href={menu.path} className="rounded-2xl hover:bg-light-pink cursor-pointer p-4 flex flex-col items-center gap-2">
               <div className="text-2xl">{menu.icon}</div> <span className="text-xs">{menu.title}</span>
-            </div>
+            </Link>
           </li>
         ))}
       </ul>
