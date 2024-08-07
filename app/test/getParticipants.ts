a2c8261b8a32fb9f190c559b68a7d294906556ce
Marjorieccc <marjorie.cc.cheung@gmail.com>
2024-08-07 18:03:48 -0400

    add get participants by courses function

--- app/test/getParticipants.ts
+++ app/test/getParticipants.ts
@@ -1,7 +1,9 @@
+import { Types } from 'mongoose';
 import { connectDb } from '@/lib/connection';
 import Logging from '@/logging/logging';
 import { IStudent } from '@/lib/schema/StudentSchema';
-import { getStudents } from '@/lib/query/student';
+import { getStudents, searchStudentsByClass } from '@/lib/query/student';
+import { searchCourseById } from '@/lib/query/course';
 
 export async function getAllParticipants(): Promise<IStudent[] | null> {
   try {
@@ -9,7 +11,36 @@ export async function getAllParticipants(): Promise<IStudent[] | null> {
     const studentList = await getStudents();
     return studentList;
   } catch (error) {
-    Logging.error(`Database connection error: ${error}`);
+    Logging.error(`Data Fetching error: ${error}`);
+    return null;
+  }
+}
+
+export async function getParticipantsByCourse(
+  courseId: string
+): Promise<IStudent[] | null> {
+  try {
+    await connectDb();
+    const course = await searchCourseById(new Types.ObjectId(courseId));
+    if (course) {
+      const studentList = await Promise.all(
+        course?.classes.map(
+          async (classObj) => await searchStudentsByClass(classObj.classId)
+        )
+      );
+      // Flatten the array and remove null values
+      const flattenedStudentList = studentList
+        .flat()
+        .filter((student) => student != null);
+      // Remove duplicate students
+      if (flattenedStudentList.length > 0) {
+        const uniqueStudentList = Array.from(new Set(flattenedStudentList));
+        return uniqueStudentList;
+      }
+    }
+    return null;
+  } catch (error) {
+    Logging.error(`Data Fetching error: ${error}`);
     return null;
   }
 }
