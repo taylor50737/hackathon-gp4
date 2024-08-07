062df3e635d765475a5673fde11312a6ce1573b5
Marjorie <>
2024-08-07 03:34:14 -0400

    integrate with backend

---
+++ lib/connection.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import Logging from '@/logging/logging';
+import { config } from './config';
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectDb() {
+  try {
+    // Use existing connection if a connection already created
+    if (cached.conn) {
+      return cached.conn;
+    }
+
+    // Create new connection
+    cached.promise = mongoose.connect(config.mongo.uri, {
+      retryWrites: true,
+      w: 'majority',
+      maxPoolSize: 1,
+      minPoolSize: 1,
+      socketTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 10000,
+      maxIdleTimeMS: 10000,
+    });
+
+    cached.conn = await cached.promise;
+
+    Logging.info('Connected to MongoDB successfully.');
+    return cached.conn;
+  } catch (error) {
+    Logging.error(`Error connecting to MongoDB: ${error}`);
+    throw new Error('Database connection failed.');
+  }
+}
+
+export function disconnectDb() {
+  if (!cached.conn) {
+    return;
+  }
+  cached.conn = null;
+  mongoose.disconnect();
+  Logging.info('Database connection closed.');
+}
