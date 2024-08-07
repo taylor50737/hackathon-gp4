5942176fae55a6d2c21faafa0ada355b1fdf625e
Taylor Lo <taylorloph@gmail.com>
2024-08-07 16:31:37 -0400

    Completed course table backend merge

--- app/course-list/[courseSlug]/page.tsx
+++ app/course-list/[courseSlug]/page.tsx
@@ -1,5 +1,13 @@
-export default function CourseDetailsPage() {
-    return (
-        <div></div>
-    );
+import { getClassByCourses } from "@/lib/api/getClass";
+import { ClassDetails } from "@/lib/api/type";
+import ClassTable from "./class-table";
+
+export default async function CourseDetailPage() {
+  const courseId = "64c0f8bdfd6f9d3c123f45ab"; // hardcode courseId...
+  let classList: ClassDetails[] | null = (await getClassByCourses(courseId)) ?? [];
+  return (
+    <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4">
+        <ClassTable classList={classList} />
+    </div>
+  );
 }
