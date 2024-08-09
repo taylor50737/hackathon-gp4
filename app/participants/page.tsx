524d999265f56bb7e369e690a58ac0191c2777e2
taylor50737 <taylorloph@gmail.com>
2024-08-09 01:46:28 -0400

    Completed all participants list fe be integration

--- app/participants/page.tsx
+++ app/participants/page.tsx
@@ -1,11 +1,16 @@
 import SubHeader from "@/components/ui/sub-header";
 import ParticipantTable from "./participant-table";
+import { getAllParticipants } from "@/lib/api/getParticipants";
+import { restructureStudentData } from "./restructureData";
 
-export default function AllParticipantsPage() {
+export default async function AllParticipantsPage() {
+  const participantList = (await getAllParticipants()) ?? [];
+  const restructuredParticipantList = restructureStudentData(participantList);
+
   return (
     <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4">
       <SubHeader header="All Participants" />
-      <ParticipantTable />
+      <ParticipantTable participantList={restructuredParticipantList} />
     </div>
   );
 }
