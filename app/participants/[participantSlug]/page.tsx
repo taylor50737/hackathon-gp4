978b0fa44d035c5250b3d4e0c4db76a9c5342ff1
Taylor Lo <taylorloph@gmail.com>
2024-08-09 19:04:46 -0400

    Participant page web responsive

--- app/participants/[participantSlug]/page.tsx
+++ app/participants/[participantSlug]/page.tsx
@@ -24,20 +24,22 @@ export default async function ParticipantProfilePage({
     : [];
 
   return (
-    <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4">
+    <div className="flex flex-col flex-1 py-4 h-full sm:h-fit space-y-4 px-4">
       <SubHeader header="Participant Profile" />
-      <div className="bg-light-violet">
-        <div className="flex items-center justify-center gap-10 py-10">
+      <div className="bg-light-violet p-4 rounded-lg">
+        <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
           <Image src={avatar1} alt="avatar1" width={80} />
-          <div className="flex flex-col gap-2">
-            <h1 className="font-bold text-xl">
+          <div className="flex flex-col items-center sm:items-start gap-2">
+            <h1 className="font-bold text-xl text-center sm:text-left">
               {participant?.firstName} {participant?.lastName}
             </h1>
-            <h1>Student ID# {participant?._id.toString()}</h1>
+            <h1 className="text-center sm:text-left">
+              Student ID# {participant?._id.toString()}
+            </h1>
           </div>
-          <div className="text-xs">
+          <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
             <p>
-              <b>Date of Birth:</b>
+              <b>Date of Birth:</b>{" "}
               <span>
                 {participant?.dob
                   ? participant.dob.toLocaleDateString()
@@ -58,22 +60,22 @@ export default async function ParticipantProfilePage({
             </p>
             <p>
               <b>Remaining Credit:</b>{" "}
-              <span>{participant?.credits ? participant?.credits : "N/A"}</span>
+              <span>{participant?.credits ? participant.credits : "N/A"}</span>
             </p>
           </div>
         </div>
       </div>
 
-      <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2 ">
-        <div className="bg-light-gray">
-          <div className="flex items-center justify-center gap-10 py-10">
-            <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-xl">
+      <div className="grid gap-4 sm:grid-cols-1 lg:grid-cols-2">
+        <div className="bg-light-gray p-4 rounded-lg">
+          <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
+            <div className="flex flex-col items-center sm:items-start gap-2">
+              <h1 className="font-bold text-xl text-center sm:text-left">
                 {participant?.guardian?.fullName}
               </h1>
-              <h1>Guardian</h1>
+              <h1 className="text-center sm:text-left">Guardian</h1>
             </div>
-            <div className="text-xs">
+            <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
               <p>
                 <b>Relationship:</b>{" "}
                 <span>{participant?.guardian?.relationship}</span>
@@ -87,21 +89,22 @@ export default async function ParticipantProfilePage({
             </div>
           </div>
         </div>
-        <div className="bg-light-gray">
-          <div className="flex items-center justify-center gap-10 py-10">
-            <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-xl">
-                {participant?.emergency.fullName}
+
+        <div className="bg-light-gray p-4 rounded-lg">
+          <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
+            <div className="flex flex-col items-center sm:items-start gap-2">
+              <h1 className="font-bold text-xl text-center sm:text-left">
+                {participant?.emergency?.fullName}
               </h1>
-              <h1>Emergency</h1>
+              <h1 className="text-center sm:text-left">Emergency</h1>
             </div>
-            <div className="text-xs">
+            <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
               <p>
                 <b>Relationship:</b>{" "}
-                <span>{participant?.emergency.relationship}</span>
+                <span>{participant?.emergency?.relationship}</span>
               </p>
               <p>
-                <b>Phone:</b> <span>{participant?.emergency.phone}</span>
+                <b>Phone:</b> <span>{participant?.emergency?.phone}</span>
               </p>
               <p>
                 <b>Email:</b> <span>abc@gmail.com</span>
