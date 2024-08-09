6e53242cba747e0ae2e1acdf7a9c9a2bda10efa7
taylor50737 <taylorloph@gmail.com>
2024-08-09 18:42:27 -0400

    Added mobile web responsivess and menu

--- components/ui/search-input.tsx
+++ components/ui/search-input.tsx
@@ -2,16 +2,42 @@
 
 import { Input } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 import { search } from "@/actions/search";
 
 export default function SearchInput() {
-  const SearchParams = useSearchParams();
+  const searchParams = useSearchParams();
+  const [placeholder, setPlaceholder] = useState(
+    "Search course name, class name or participant name..."
+  );
+
+  useEffect(() => {
+    const updatePlaceholder = () => {
+      if (window.innerWidth < 1024) {
+        // lg breakpoint in Tailwind CSS
+        setPlaceholder("Search...");
+      } else {
+        setPlaceholder("Search course name, class name or participant name...");
+      }
+    };
+
+    // Set the initial placeholder based on the current window size
+    updatePlaceholder();
+
+    // Update placeholder when resizing
+    window.addEventListener("resize", updatePlaceholder);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", updatePlaceholder);
+  }, []);
+
   return (
     <form action={search} className="w-full">
       <Input
         name="term"
-        defaultValue={SearchParams.get("term") || ""}
-        placeholder="Search by course name, class name or participant name..."
+        defaultValue={searchParams.get("term") || ""}
+        placeholder={placeholder}
+        className="truncate"
       />
     </form>
   );
