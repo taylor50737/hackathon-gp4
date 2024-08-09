93a43856032b5b50e29cdbe612f2cf6c9330e970
taylor50737 <taylorloph@gmail.com>
2024-08-09 13:01:38 -0400

    Fixed type error

--- lib/query/search.ts
+++ lib/query/search.ts
@@ -1,3 +1,5 @@
+import { connectDb } from "@/lib/connection";
+import Logging from "@/logging/logging";
 import { searchCourseByName } from "./course";
 import { searchClassByName } from "./classes";
 import { searchStudentByName } from "./student";
@@ -8,11 +10,21 @@ import {
 } from "./restructureData";
 
 export async function fetchBySearchTerm(term: string) {
-  const [students, courses, classes] = await Promise.all([
-    searchStudentByName(term),
-    searchCourseByName(term),
-    searchClassByName(term),
-  ]);
+    let students = [], courses = [], classes = []; // Declare outside to widen scope
+    try {
+      await connectDb();
+      const results = await Promise.all([
+        searchStudentByName(term),
+        searchCourseByName(term),
+        searchClassByName(term),
+      ]);
+      students = results[0] || [];
+      courses = results[1] || [];
+      classes = results[2] || [];
+    } catch (error) {
+      Logging.error(`Data Fetching error: ${error}`);
+      return null;
+    }
 
   const restructuredCourses = restructureCourseData(courses || []);
   const restructuredClasses = restructureClassData(classes || []);
