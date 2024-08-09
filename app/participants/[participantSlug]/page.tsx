7dcf6f5ba81cd96bbcf7eecf67ee9cd234d25359
Taylor Lo <taylorloph@gmail.com>
2024-08-09 01:29:17 -0400

    Fixed footer layout and completed participantSlug screen

--- app/participants/[participantSlug]/page.tsx
+++ app/participants/[participantSlug]/page.tsx
@@ -1,15 +1,28 @@
 import Image from "next/image";
-import Link from "next/link";
 import { Types } from "mongoose";
 
 import SubHeader from "@/components/ui/sub-header";
 import ParticipantCourseTable from "./course-table";
 import avatar1 from "public/avatar_1.png";
-import { getEnrolledClassByStudentId } from "@/lib/api/getClass";
+import { searchStudentById } from "@/lib/query/student";
+import {
+  IRestructuredClassEnrolled,
+  restructureEnrolledClassData,
+} from "./restructureData";
 
-export default async function ParticipantProfilePage() {
-  const enrolledClassList = await getEnrolledClassByStudentId(new Types.ObjectId("64c0f90a7e6b9e2b90234567"));
-  console.log(enrolledClassList);
+export default async function ParticipantProfilePage({
+  params,
+}: {
+  params: { participantSlug: string };
+}) {
+  const participant = await searchStudentById(
+    new Types.ObjectId("64c0f90a7e6b9e2b90234567")
+  );
+
+  const restructuredEnrolledClassList = participant
+    ? restructureEnrolledClassData(participant.classEnrolled)
+    : [];
+
   return (
     <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4">
       <SubHeader header="Participant Profile" />
@@ -17,30 +30,35 @@ export default async function ParticipantProfilePage() {
         <div className="flex items-center justify-center gap-10 py-10">
           <Image src={avatar1} alt="avatar1" width={80} />
           <div className="flex flex-col gap-2">
-            <h1 className="font-bold text-xl">Matt Dickerson</h1>
-            <h1>Student ID# 00001</h1>
+            <h1 className="font-bold text-xl">
+              {participant?.firstName} {participant?.lastName}
+            </h1>
+            <h1>Student ID# {participant?._id.toString()}</h1>
           </div>
           <div className="text-xs">
             <p>
-              <b>Date of Birth:</b> <span>13-05-2008</span>
+              <b>Date of Birth:</b>
+              <span>
+                {participant?.dob
+                  ? participant.dob.toLocaleDateString()
+                  : "N/A"}
+              </span>
             </p>
             <p>
-              <b>Phone:</b> <span>647-123-4567</span>
+              <b>Phone:</b> <span>{participant?.phone}</span>
             </p>
             <p>
-              <b>Email:</b> <span>cics@gmail.com</span>
+              <b>Email:</b> <span>{participant?.email}</span>
             </p>
             <p>
-              <b>Address:</b> <span>4002 Sheppard Ave E Suite</span>
+              <b>Address:</b> <span>{participant?.address}</span>
             </p>
             <p>
-              <b>Special Needs:</b> <span>ADHD</span>
+              <b>Special Needs:</b> <span>{participant?.specialNeeds}</span>
             </p>
             <p>
-              <b>Language:</b> <span>French</span>
-            </p>
-            <p>
-              <b>Remaining Credit:</b> <span>250</span>
+              <b>Remaining Credit:</b>{" "}
+              <span>{participant?.credits ? participant?.credits : "N/A"}</span>
             </p>
           </div>
         </div>
@@ -50,18 +68,18 @@ export default async function ParticipantProfilePage() {
         <div className="bg-light-gray">
           <div className="flex items-center justify-center gap-10 py-10">
             <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-xl">Matt Sr.</h1>
-              <h1>Student ID# 00001</h1>
+              <h1 className="font-bold text-xl">
+                {participant?.guardian?.fullName}
+              </h1>
+              <h1>Guardian</h1>
             </div>
             <div className="text-xs">
               <p>
-                <b>Relationship:</b> <span>Father</span>
+                <b>Relationship:</b>{" "}
+                <span>{participant?.guardian?.relationship}</span>
               </p>
               <p>
-                <b>Phone:</b> <span>647-123-6789</span>
-              </p>
-              <p>
-                <b>Email:</b> <span>mattsr@gmail.com</span>
+                <b>Email:</b> <span>abc@gmail.com</span>
               </p>
               <p>
                 <b>Language:</b> <span>English</span>
@@ -72,18 +90,21 @@ export default async function ParticipantProfilePage() {
         <div className="bg-light-gray">
           <div className="flex items-center justify-center gap-10 py-10">
             <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-xl">Lucinda Smith</h1>
-              <h1>Student ID# 00001</h1>
+              <h1 className="font-bold text-xl">
+                {participant?.emergency.fullName}
+              </h1>
+              <h1>Emergency</h1>
             </div>
             <div className="text-xs">
               <p>
-                <b>Relationship:</b> <span>Mother</span>
+                <b>Relationship:</b>{" "}
+                <span>{participant?.emergency.relationship}</span>
               </p>
               <p>
-                <b>Phone:</b> <span>647-123-2468</span>
+                <b>Phone:</b> <span>{participant?.emergency.phone}</span>
               </p>
               <p>
-                <b>Email:</b> <span>lsmith@gmail.com</span>
+                <b>Email:</b> <span>abc@gmail.com</span>
               </p>
               <p>
                 <b>Language:</b> <span>English</span>
@@ -92,7 +113,7 @@ export default async function ParticipantProfilePage() {
           </div>
         </div>
       </div>
-      <ParticipantCourseTable />
+      <ParticipantCourseTable courses={restructuredEnrolledClassList} />
     </div>
   );
 }
