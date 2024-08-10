f3867a71cfc1ec9602e06e088094199a208a42a6
Marjorie <>
2024-08-10 03:35:13 -0400

    fix server-side exception error

--- app/course-list/[courseSlug]/[classSlug]/page.tsx
+++ app/course-list/[courseSlug]/[classSlug]/page.tsx
@@ -1,16 +1,19 @@
-import { searchStudentsByClass } from "@/lib/query/student";
-import { Types } from "mongoose";
-import ParticipantTable from "./participant-table";
-import { restructureStudentData } from "./restructureData";
-import SubHeader from "@/components/ui/sub-header";
-import { searchCourseById } from "@/lib/query/course";
-import { searchClassById } from "@/lib/query/classes";
+import { searchStudentsByClass } from '@/lib/query/student';
+import { Types } from 'mongoose';
+import ParticipantTable from './participant-table';
+import { restructureStudentData } from './restructureData';
+import SubHeader from '@/components/ui/sub-header';
+import { searchCourseById } from '@/lib/query/course';
+import { searchClassById } from '@/lib/query/classes';
+import { connectDb } from '@/lib/connection';
 
 export default async function CourseDetailPage({
   params,
 }: {
   params: { courseSlug: string; classSlug: string };
 }) {
+  await connectDb();
+
   const courseId = new Types.ObjectId(params.courseSlug);
   const classId = new Types.ObjectId(params.classSlug);
 
@@ -18,15 +21,15 @@ export default async function CourseDetailPage({
   const classItem = await searchClassById(classId);
 
   const courseName = course?.name;
-  const className = classItem?.name
+  const className = classItem?.name;
 
   const participantList = (await searchStudentsByClass(classId)) ?? [];
   const restructuredParticipantList = restructureStudentData(participantList);
 
   return (
-    <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4">
+    <div className='flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4 gap-4'>
       <SubHeader header={courseName} subHeader={className} />
-      <div className="pt-10">
+      <div className='pt-10'>
         <ParticipantTable participantList={restructuredParticipantList} />
       </div>
     </div>
--- app/participants/[participantSlug]/page.tsx
+++ app/participants/[participantSlug]/page.tsx
@@ -1,20 +1,22 @@
-import Image from "next/image";
-import { Types } from "mongoose";
+import Image from 'next/image';
+import { Types } from 'mongoose';
 
-import SubHeader from "@/components/ui/sub-header";
-import ParticipantCourseTable from "./course-table";
-import avatar1 from "public/avatar_1.png";
-import { searchStudentById } from "@/lib/query/student";
+import SubHeader from '@/components/ui/sub-header';
+import ParticipantCourseTable from './course-table';
+import avatar1 from 'public/avatar_1.png';
+import { searchStudentById } from '@/lib/query/student';
 import {
   IRestructuredClassEnrolled,
   restructureEnrolledClassData,
-} from "./restructureData";
+} from './restructureData';
+import { connectDb } from '@/lib/connection';
 
 export default async function ParticipantProfilePage({
   params,
 }: {
   params: { participantSlug: string };
 }) {
+  await connectDb();
   const participant = await searchStudentById(
     new Types.ObjectId(params.participantSlug)
   );
@@ -24,26 +26,26 @@ export default async function ParticipantProfilePage({
     : [];
 
   return (
-    <div className="flex flex-col flex-1 py-4 h-full sm:h-fit space-y-4 px-4">
-      <SubHeader header="Participant Profile" />
-      <div className="bg-light-violet p-4 rounded-lg">
-        <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
-          <Image src={avatar1} alt="avatar1" width={80} />
-          <div className="flex flex-col items-center sm:items-start gap-2">
-            <h1 className="font-bold text-xl text-center sm:text-left">
+    <div className='flex flex-col flex-1 py-4 h-full sm:h-fit space-y-4 px-4'>
+      <SubHeader header='Participant Profile' />
+      <div className='bg-light-violet p-4 rounded-lg'>
+        <div className='flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6'>
+          <Image src={avatar1} alt='avatar1' width={80} />
+          <div className='flex flex-col items-center sm:items-start gap-2'>
+            <h1 className='font-bold text-xl text-center sm:text-left'>
               {participant?.firstName} {participant?.lastName}
             </h1>
-            <h1 className="text-center sm:text-left">
+            <h1 className='text-center sm:text-left'>
               Student ID# {participant?._id.toString()}
             </h1>
           </div>
-          <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
+          <div className='text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1'>
             <p>
-              <b>Date of Birth:</b>{" "}
+              <b>Date of Birth:</b>{' '}
               <span>
                 {participant?.dob
                   ? participant.dob.toLocaleDateString()
-                  : "N/A"}
+                  : 'N/A'}
               </span>
             </p>
             <p>
@@ -59,25 +61,25 @@ export default async function ParticipantProfilePage({
               <b>Special Needs:</b> <span>{participant?.specialNeeds}</span>
             </p>
             <p>
-              <b>Remaining Credit:</b>{" "}
-              <span>{participant?.credits ? participant.credits : "N/A"}</span>
+              <b>Remaining Credit:</b>{' '}
+              <span>{participant?.credits ? participant.credits : 'N/A'}</span>
             </p>
           </div>
         </div>
       </div>
 
-      <div className="grid gap-4 sm:grid-cols-1 lg:grid-cols-2">
-        <div className="bg-light-gray p-4 rounded-lg">
-          <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
-            <div className="flex flex-col items-center sm:items-start gap-2">
-              <h1 className="font-bold text-xl text-center sm:text-left">
+      <div className='grid gap-4 sm:grid-cols-1 lg:grid-cols-2'>
+        <div className='bg-light-gray p-4 rounded-lg'>
+          <div className='flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6'>
+            <div className='flex flex-col items-center sm:items-start gap-2'>
+              <h1 className='font-bold text-xl text-center sm:text-left'>
                 {participant?.guardian?.fullName}
               </h1>
-              <h1 className="text-center sm:text-left">Guardian</h1>
+              <h1 className='text-center sm:text-left'>Guardian</h1>
             </div>
-            <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
+            <div className='text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1'>
               <p>
-                <b>Relationship:</b>{" "}
+                <b>Relationship:</b>{' '}
                 <span>{participant?.guardian?.relationship}</span>
               </p>
               <p>
@@ -90,17 +92,17 @@ export default async function ParticipantProfilePage({
           </div>
         </div>
 
-        <div className="bg-light-gray p-4 rounded-lg">
-          <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6">
-            <div className="flex flex-col items-center sm:items-start gap-2">
-              <h1 className="font-bold text-xl text-center sm:text-left">
+        <div className='bg-light-gray p-4 rounded-lg'>
+          <div className='flex flex-col sm:flex-row items-center justify-center sm:justify-center gap-6 sm:gap-10 py-6'>
+            <div className='flex flex-col items-center sm:items-start gap-2'>
+              <h1 className='font-bold text-xl text-center sm:text-left'>
                 {participant?.emergency?.fullName}
               </h1>
-              <h1 className="text-center sm:text-left">Emergency</h1>
+              <h1 className='text-center sm:text-left'>Emergency</h1>
             </div>
-            <div className="text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1">
+            <div className='text-xs flex flex-col items-center sm:items-start space-y-2 sm:space-y-1'>
               <p>
-                <b>Relationship:</b>{" "}
+                <b>Relationship:</b>{' '}
                 <span>{participant?.emergency?.relationship}</span>
               </p>
               <p>
